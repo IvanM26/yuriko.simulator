e151e88ac289748c81a88b3656bccb9c717c2032
Ivan Millanes <ivanmillanes26@gmail.com>
2024-07-11 09:03:13 -0300

    Allow users to specify cards that must be in hand

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,21 @@
 #' @export
-draw_seven <- function(decklist){
-  decklist %>%
-    dplyr::slice_sample(n = 7)
+draw_seven <- function(decklist, must_have = NULL){
+  if (is.null(must_have)) {
+
+    decklist %>%
+      dplyr::slice_sample(n = 7)
+
+  } else {
+    n_must_have <- length(must_have)
+    
+    hand <- decklist %>%
+      dplyr::filter(!card_name %in% must_have) %>%
+      dplyr::slice_sample(n = 7 - n_must_have)
+    
+    hand %>%
+      dplyr::bind_rows(decklist %>% dplyr::filter(card_name %in% must_have))
+  }
+
 }
 
 #' @export
