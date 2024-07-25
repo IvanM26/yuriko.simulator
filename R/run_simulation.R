3b3e14d596b3b253cbb7ab0ddb7c9aea40a419f3
Ivan Millanes <ivanmillanes26@gmail.com>
2024-07-25 11:45:48 -0300

    Add run_simulation function

---
+++ R/run_simulation.R
@@ -0,0 +1,29 @@
+#' @export
+run_simulation <- function(decklist, fixed_cards = NULL, seed = 2611, n_sim) {
+  # Set seet for reproducibility
+  set.seed(seed)
+  
+  # Initialize count of yuriko triggers on turn two
+  n_yuriko_triggers_on_turn_two <- 0
+  
+  # Initialize progress bar
+  cli::cli_progress_bar("Running Simulation...", total = n_sim, format = paste0("ETA:{pb_eta}"))
+  
+  # Simulate...
+  for (i in 1:n_sim) {
+    
+    hand <- draw_seven(decklist, fixed_cards)
+    
+    if (is_yuriko_on_T2(hand)) n_yuriko_triggers_on_turn_two = n_yuriko_triggers_on_turn_two + 1
+    
+    cli::cli_progress_update()
+    
+  }
+  
+  cli::cli_progress_done()
+  
+  prob_yuriko_triggers_on_turn_two <- n_yuriko_triggers_on_turn_two / n_sim
+  
+  return(prob_yuriko_triggers_on_turn_two)
+  
+}
